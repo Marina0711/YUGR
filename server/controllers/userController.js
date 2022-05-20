0a8c8a7101a024f4437465016dccd8148c5c96ea
Marina Migova <migova.exceedteam@gmail.com>
2022-05-20 12:59:24 +0300

    Fix userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -32,9 +32,16 @@ class UserController {
             const hashPassword = await  bcrypt.hash(password, 5)
             const newUser = await User.create({firstName, lastName, phoneNumber, email, role, password: hashPassword})
             await Order.create({ userId: newUser.id })
-            const token = generateJwt(newUser.id, newUser.email, newUser.role)
+            const token = generateJwt(
+                newUser.id,
+                newUser.email,
+                newUser.role,
+                newUser.firstName,
+                newUser.lastName,
+                newUser.phoneNumber
+            )
 
-            return res.json({token})
+            return res.json({ token })
         } catch (e) {
             next(ApiError.badRequest((e.message)))
         }
@@ -53,8 +60,16 @@ class UserController {
                 return next(ApiError.internal('Указан неверный пароль!'))
             }
 
-            const token = generateJwt(user.id, user.email, user.role)
-            return res.json({token})
+            const token = generateJwt(
+                user.id,
+                user.email,
+                user.role,
+                user.firstName,
+                user.lastName,
+                user.phoneNumber
+            )
+
+            return res.json({ token })
         } catch (e) {
             next(ApiError.badRequest((e.message)))
         }
@@ -62,7 +77,14 @@ class UserController {
 
     async refreshToken(req, res, next) {
         try {
-            const token = generateJwt(req.user.id, req.user.email, req.user.role)
+            const token = generateJwt(
+                req.user.id,
+                req.user.email,
+                req.user.role,
+                req.user.firstName,
+                req.user.lastName,
+                req.user.phoneNumber
+            )
             res.json({token})
         } catch (e) {
             next(ApiError.badRequest((e.message)))
