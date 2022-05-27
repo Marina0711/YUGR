5c939d42ddcb14ce64418c097b530edac2c778c4
Marina Migova <migova.exceedteam@gmail.com>
2022-05-27 11:45:23 +0300

    Fix user card

--- client/src/screens/UserScreen.tsx
+++ client/src/screens/UserScreen.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/EvilIcons';
 
+import { observer } from 'mobx-react-lite';
+
 import { HeaderComponent } from '../components/HeaderComponent';
 
 import { Colors } from '../assets/Colors';
 import { Strings } from '../assets/Strings';
+import { userStore } from '../store/UserStore';
 
 const ICON_NAME = 'user';
 const ICON_SIZE = 110;
 
-export const UserScreen = () => {
+export const UserScreen = observer(() => {
+    const { firstName, lastName, email, phoneNumber } = userStore.user!;
+
     return (
         <SafeAreaView style={styles.container}>
             <HeaderComponent isLogoutButton title={Strings.userScreen.profile} />
@@ -18,14 +23,15 @@ export const UserScreen = () => {
                 <View style={styles.userCard}>
                     <Icon name={ICON_NAME} size={ICON_SIZE} color={Colors.tin} />
                     <View>
-                        <Text style={styles.name}>Фамилия Имя</Text>
-                        <Text style={styles.email}>email@email.com</Text>
+                        <Text style={styles.name}>{firstName} {lastName}</Text>
+                        <Text style={styles.personalData}>{email}</Text>
+                        <Text style={styles.personalData}>{phoneNumber}</Text>
                     </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
     );
-};
+});
 
 const styles = StyleSheet.create({
     container:{
@@ -42,12 +48,12 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         paddingVertical: 5
     },
-    email: {
+    personalData: {
         fontSize: 14,
         marginTop: 3,
         color: Colors.tin
     },
     name: {
-        fontSize: 18
+        fontSize: 20
     }
 });
